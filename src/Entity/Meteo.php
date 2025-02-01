33ef7133c88e344b2767502f25738264d7afcac1
Anton Riviere <163549884+AntonRiviere@users.noreply.github.com>
2025-02-01 16:46:10 +0100

    Adding meteo form

--- src/Entity/Meteo.php
+++ src/Entity/Meteo.php
@@ -17,77 +17,519 @@ class Meteo
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="integer")
      */
-    private $textBox1;
+    private $fileYears;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="float")
      */
-    private $textBox2;
+    private $sodiumChlorideConcentration;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $waterFilmThickness;
 
     /**
      * @ORM\Column(type="integer")
      */
-    private $numericUpDown1;
+    private $humidityThreshold;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $mechanicalAnnualSodium;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $mechanicalMeanSodium;
 
     /**
      * @ORM\Column(type="integer")
      */
-    private $numericUpDown2;
+    private $mechanicalInterventions;
 
-    // Add additional properties as needed for the other fields
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $mechanicalInterval;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $mechanicalSodiumWater;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $mechanicalThresholdTemperature;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $automaticAnnualSodium;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $automaticMeanSodium;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $automaticSprays;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $automaticSprayInterval;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $automaticSodiumWater;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $automaticThresholdTemperature;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $extTemperaturePosition;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $extTemperatureDivider;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $extTemperatureAttenuation;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $extTemperatureLimit;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $extHumidityPosition;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $extHumidityDivider;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $extHumidityAttenuation;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $extHumidityLimit;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $intTemperaturePosition;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $intTemperatureDivider;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $intTemperatureAttenuation;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $intTemperatureLimit;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $intHumidityPosition;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $intHumidityDivider;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $intHumidityAttenuation;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $intHumidityLimit;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getTextBox1(): ?string
+    public function getFileYears(): ?int
     {
-        return $this->textBox1;
+        return $this->fileYears;
     }
 
-    public function setTextBox1(string $textBox1): self
+    public function setFileYears(int $fileYears): self
     {
-        $this->textBox1 = $textBox1;
-
+        $this->fileYears = $fileYears;
         return $this;
     }
 
-    public function getTextBox2(): ?string
+    public function getSodiumChlorideConcentration(): ?float
     {
-        return $this->textBox2;
+        return $this->sodiumChlorideConcentration;
     }
 
-    public function setTextBox2(string $textBox2): self
+    public function setSodiumChlorideConcentration(float $sodiumChlorideConcentration): self
     {
-        $this->textBox2 = $textBox2;
-
+        $this->sodiumChlorideConcentration = $sodiumChlorideConcentration;
         return $this;
     }
 
-    public function getNumericUpDown1(): ?int
+    public function getWaterFilmThickness(): ?float
     {
-        return $this->numericUpDown1;
+        return $this->waterFilmThickness;
     }
 
-    public function setNumericUpDown1(int $numericUpDown1): self
+    public function setWaterFilmThickness(float $waterFilmThickness): self
     {
-        $this->numericUpDown1 = $numericUpDown1;
-
+        $this->waterFilmThickness = $waterFilmThickness;
         return $this;
     }
 
-    public function getNumericUpDown2(): ?int
+    public function getHumidityThreshold(): ?int
     {
-        return $this->numericUpDown2;
+        return $this->humidityThreshold;
     }
 
-    public function setNumericUpDown2(int $numericUpDown2): self
+    public function setHumidityThreshold(int $humidityThreshold): self
     {
-        $this->numericUpDown2 = $numericUpDown2;
+        $this->humidityThreshold = $humidityThreshold;
+        return $this;
+    }
 
+    public function getMechanicalAnnualSodium(): ?float
+    {
+        return $this->mechanicalAnnualSodium;
+    }
+
+    public function setMechanicalAnnualSodium(float $mechanicalAnnualSodium): self
+    {
+        $this->mechanicalAnnualSodium = $mechanicalAnnualSodium;
+        return $this;
+    }
+
+    public function getMechanicalMeanSodium(): ?float
+    {
+        return $this->mechanicalMeanSodium;
+    }
+
+    public function setMechanicalMeanSodium(float $mechanicalMeanSodium): self
+    {
+        $this->mechanicalMeanSodium = $mechanicalMeanSodium;
+        return $this;
+    }
+
+    public function getMechanicalInterventions(): ?int
+    {
+        return $this->mechanicalInterventions;
+    }
+
+    public function setMechanicalInterventions(int $mechanicalInterventions): self
+    {
+        $this->mechanicalInterventions = $mechanicalInterventions;
+        return $this;
+    }
+
+    public function getMechanicalInterval(): ?float
+    {
+        return $this->mechanicalInterval;
+    }
+
+    public function setMechanicalInterval(float $mechanicalInterval): self
+    {
+        $this->mechanicalInterval = $mechanicalInterval;
+        return $this;
+    }
+
+    public function getMechanicalSodiumWater(): ?int
+    {
+        return $this->mechanicalSodiumWater;
+    }
+
+    public function setMechanicalSodiumWater(int $mechanicalSodiumWater): self
+    {
+        $this->mechanicalSodiumWater = $mechanicalSodiumWater;
+        return $this;
+    }
+
+    public function getMechanicalThresholdTemperature(): ?float
+    {
+        return $this->mechanicalThresholdTemperature;
+    }
+
+    public function setMechanicalThresholdTemperature(?float $mechanicalThresholdTemperature): self
+    {
+        $this->mechanicalThresholdTemperature = $mechanicalThresholdTemperature;
+        return $this;
+    }
+
+    public function getAutomaticAnnualSodium(): ?float
+    {
+        return $this->automaticAnnualSodium;
+    }
+
+    public function setAutomaticAnnualSodium(float $automaticAnnualSodium): self
+    {
+        $this->automaticAnnualSodium = $automaticAnnualSodium;
+        return $this;
+    }
+
+    public function getAutomaticMeanSodium(): ?float
+    {
+        return $this->automaticMeanSodium;
+    }
+
+    public function setAutomaticMeanSodium(float $automaticMeanSodium): self
+    {
+        $this->automaticMeanSodium = $automaticMeanSodium;
+        return $this;
+    }
+
+    public function getAutomaticSprays(): ?int
+    {
+        return $this->automaticSprays;
+    }
+
+    public function setAutomaticSprays(int $automaticSprays): self
+    {
+        $this->automaticSprays = $automaticSprays;
+        return $this;
+    }
+
+    public function getAutomaticSprayInterval(): ?int
+    {
+        return $this->automaticSprayInterval;
+    }
+
+    public function setAutomaticSprayInterval(int $automaticSprayInterval): self
+    {
+        $this->automaticSprayInterval = $automaticSprayInterval;
+        return $this;
+    }
+
+    public function getAutomaticSodiumWater(): ?int
+    {
+        return $this->automaticSodiumWater;
+    }
+
+    public function setAutomaticSodiumWater(int $automaticSodiumWater): self
+    {
+        $this->automaticSodiumWater = $automaticSodiumWater;
+        return $this;
+    }
+
+    public function getAutomaticThresholdTemperature(): ?float
+    {
+        return $this->automaticThresholdTemperature;
+    }
+
+    public function setAutomaticThresholdTemperature(?float $automaticThresholdTemperature): self
+    {
+        $this->automaticThresholdTemperature = $automaticThresholdTemperature;
+        return $this;
+    }
+
+    public function getExtTemperaturePosition(): ?int
+    {
+        return $this->extTemperaturePosition;
+    }
+
+    public function setExtTemperaturePosition(int $extTemperaturePosition): self
+    {
+        $this->extTemperaturePosition = $extTemperaturePosition;
+        return $this;
+    }
+
+    public function getExtTemperatureDivider(): ?int
+    {
+        return $this->extTemperatureDivider;
+    }
+
+    public function setExtTemperatureDivider(int $extTemperatureDivider): self
+    {
+        $this->extTemperatureDivider = $extTemperatureDivider;
+        return $this;
+    }
+
+    public function getExtTemperatureAttenuation(): ?int
+    {
+        return $this->extTemperatureAttenuation;
+    }
+
+    public function setExtTemperatureAttenuation(int $extTemperatureAttenuation): self
+    {
+        $this->extTemperatureAttenuation = $extTemperatureAttenuation;
+        return $this;
+    }
+
+    public function getExtTemperatureLimit(): ?float
+    {
+        return $this->extTemperatureLimit;
+    }
+
+    public function setExtTemperatureLimit(float $extTemperatureLimit): self
+    {
+        $this->extTemperatureLimit = $extTemperatureLimit;
+        return $this;
+    }
+
+    public function getExtHumidityPosition(): ?int
+    {
+        return $this->extHumidityPosition;
+    }
+
+    public function setExtHumidityPosition(int $extHumidityPosition): self
+    {
+        $this->extHumidityPosition = $extHumidityPosition;
+        return $this;
+    }
+
+    public function getExtHumidityDivider(): ?int
+    {
+        return $this->extHumidityDivider;
+    }
+
+    public function setExtHumidityDivider(int $extHumidityDivider): self
+    {
+        $this->extHumidityDivider = $extHumidityDivider;
+        return $this;
+    }
+
+    public function getExtHumidityAttenuation(): ?int
+    {
+        return $this->extHumidityAttenuation;
+    }
+
+    public function setExtHumidityAttenuation(int $extHumidityAttenuation): self
+    {
+        $this->extHumidityAttenuation = $extHumidityAttenuation;
+        return $this;
+    }
+
+    public function getExtHumidityLimit(): ?float
+    {
+        return $this->extHumidityLimit;
+    }
+
+    public function setExtHumidityLimit(float $extHumidityLimit): self
+    {
+        $this->extHumidityLimit = $extHumidityLimit;
+        return $this;
+    }
+
+    public function getIntTemperaturePosition(): ?int
+    {
+        return $this->intTemperaturePosition;
+    }
+
+    public function setIntTemperaturePosition(int $intTemperaturePosition): self
+    {
+        $this->intTemperaturePosition = $intTemperaturePosition;
+        return $this;
+    }
+
+    public function getIntTemperatureDivider(): ?int
+    {
+        return $this->intTemperatureDivider;
+    }
+
+    public function setIntTemperatureDivider(int $intTemperatureDivider): self
+    {
+        $this->intTemperatureDivider = $intTemperatureDivider;
+        return $this;
+    }
+
+    public function getIntTemperatureAttenuation(): ?int
+    {
+        return $this->intTemperatureAttenuation;
+    }
+
+    public function setIntTemperatureAttenuation(int $intTemperatureAttenuation): self
+    {
+        $this->intTemperatureAttenuation = $intTemperatureAttenuation;
+        return $this;
+    }
+
+    public function getIntTemperatureLimit(): ?float
+    {
+        return $this->intTemperatureLimit;
+    }
+
+    public function setIntTemperatureLimit(float $intTemperatureLimit): self
+    {
+        $this->intTemperatureLimit = $intTemperatureLimit;
+        return $this;
+    }
+
+    public function getIntHumidityPosition(): ?int
+    {
+        return $this->intHumidityPosition;
+    }
+
+    public function setIntHumidityPosition(int $intHumidityPosition): self
+    {
+        $this->intHumidityPosition = $intHumidityPosition;
+        return $this;
+    }
+
+    public function getIntHumidityDivider(): ?int
+    {
+        return $this->intHumidityDivider;
+    }
+
+    public function setIntHumidityDivider(int $intHumidityDivider): self
+    {
+        $this->intHumidityDivider = $intHumidityDivider;
+        return $this;
+    }
+
+    public function getIntHumidityAttenuation(): ?int
+    {
+        return $this->intHumidityAttenuation;
+    }
+
+    public function setIntHumidityAttenuation(int $intHumidityAttenuation): self
+    {
+        $this->intHumidityAttenuation = $intHumidityAttenuation;
+        return $this;
+    }
+
+    public function getIntHumidityLimit(): ?float
+    {
+        return $this->intHumidityLimit;
+    }
+
+    public function setIntHumidityLimit(float $intHumidityLimit): self
+    {
+        $this->intHumidityLimit = $intHumidityLimit;
         return $this;
     }
 }
--- src/Form/MeteoType.php
+++ src/Form/MeteoType.php
@@ -6,26 +6,61 @@ use App\Entity\Meteo;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class MeteoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('textBox1', TextType::class, [
-                'label' => 'Text Box 1',
-            ])
-            ->add('textBox2', TextType::class, [
-                'label' => 'Text Box 2',
-            ])
-            ->add('numericUpDown1', IntegerType::class, [
-                'label' => 'Numeric Up Down 1',
-            ])
-            ->add('numericUpDown2', IntegerType::class, [
-                'label' => 'Numeric Up Down 2',
-            ]);
+            ->add('fileYears', NumberType::class, ['label' => 'Nombre d\'années (fichier)'])
+            ->add('sodiumChlorideConcentration', NumberType::class, ['label' => 'Concentration de chlorure de sodium au temps'])
+            ->add('waterFilmThickness', NumberType::class, ['label' => 'Épaisseur du film d\'eau sur la chaussée'])
+            ->add('humidityThreshold', IntegerType::class, ['label' => 'Humidité relative seuil d\'intervention en cas de basses températures'])
+
+            ->add('mechanicalAnnualSodium', NumberType::class, ['label' => 'Concentration annuelle en chlorure de sodium'])
+            ->add('mechanicalMeanSodium', NumberType::class, ['label' => 'Quantité moyenne de chlorure de sodium'])
+            ->add('mechanicalInterventions', IntegerType::class, ['label' => 'Nombre d\'interventions d\'épandage de chlorure de sodium (mécanique)'])
+            ->add('mechanicalInterval', NumberType::class, ['label' => 'Intervalle minimal entre épandages (mécanique)'])
+            ->add('mechanicalSodiumWater', IntegerType::class, ['label' => 'Concentration de chlorure de sodium dans l\'eau (mécanique)'])
+            ->add('mechanicalThresholdTemperature', NumberType::class, ['label' => 'Température seuil d\'intervention (mécanique)', 'required' => false])
+
+            ->add('automaticAnnualSodium', NumberType::class, ['label' => 'Concentration annuelle en chlorure de sodium (automatique)'])
+            ->add('automaticMeanSodium', NumberType::class, ['label' => 'Quantité moyenne de chlorure de sodium (automatique)'])
+            ->add('automaticSprays', IntegerType::class, ['label' => 'Nombre de giclages annuels (automatique)'])
+            ->add('automaticSprayInterval', IntegerType::class, ['label' => 'Nombre de giclages sur un intervalle de temps (automatique)'])
+            ->add('automaticSodiumWater', IntegerType::class, ['label' => 'Concentration de chlorure de sodium dans l\'eau (automatique)'])
+            ->add('automaticThresholdTemperature', NumberType::class, ['label' => 'Température seuil d\'intervention (automatique)', 'required' => false])
+
+            ->add('extTemperaturePosition', IntegerType::class, ['label' => 'Position Température extérieure'])
+            ->add('extTemperatureDivider', IntegerType::class, ['label' => 'Diviseur Température extérieure'])
+            ->add('extTemperatureAttenuation', IntegerType::class, ['label' => 'Atténuation Température extérieure'])
+            ->add('extTemperatureLimit', NumberType::class, ['label' => 'Limite Température extérieure'])
+
+            ->add('extHumidityPosition', IntegerType::class, ['label' => 'Position Humidité extérieure'])
+            ->add('extHumidityDivider', IntegerType::class, ['label' => 'Diviseur Humidité extérieure'])
+            ->add('extHumidityAttenuation', IntegerType::class, ['label' => 'Atténuation Humidité extérieure'])
+            ->add('extHumidityLimit', NumberType::class, ['label' => 'Limite Humidité extérieure'])
+
+            ->add('intTemperaturePosition', IntegerType::class, ['label' => 'Position Température intérieure'])
+            ->add('intTemperatureDivider', IntegerType::class, ['label' => 'Diviseur Température intérieure'])
+            ->add('intTemperatureAttenuation', IntegerType::class, ['label' => 'Atténuation Température intérieure'])
+            ->add('intTemperatureLimit', NumberType::class, ['label' => 'Limite Température intérieure'])
+
+            ->add('intHumidityPosition', IntegerType::class, ['label' => 'Position Humidité intérieure'])
+            ->add('intHumidityDivider', IntegerType::class, ['label' => 'Diviseur Humidité intérieure'])
+            ->add('intHumidityAttenuation', IntegerType::class, ['label' => 'Atténuation Humidité intérieure'])
+            ->add('intHumidityLimit', NumberType::class, ['label' => 'Limite Humidité intérieure'])
+
+
+            ->add('submit', SubmitType::class, [
+                'label' => 'Calculer',
+                'attr' => [
+                    'class' => 'px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+                ]
+                ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
