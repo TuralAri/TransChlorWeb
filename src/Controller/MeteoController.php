4fb070fa5f7b25d742f8b0d4b2e23354f245220c
Florian DUSSUD <137182759+Papyquirush@users.noreply.github.com>
2025-02-04 12:06:19 +0100

    traitement txt

--- src/Controller/MeteoController.php
+++ src/Controller/MeteoController.php
@@ -29,7 +29,7 @@ class MeteoController extends AbstractController
     }
 
 
-/*
+
     #Route('/meteo-form/init', name: 'meteo_form_init')
     public function init(): void
     {
@@ -47,11 +47,47 @@ class MeteoController extends AbstractController
 
         $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
+        $meteo = new Meteo();
 
+        $meteo->setFileYears($lines[0]);
+        $meteo->setSodiumChlorideConcentration($lines[1]);
+        $meteo->setWaterFilmThickness($lines[2]);
+        $meteo->setHumidityThreshold($lines[3]);
 
+        $meteo->setMechanicalAnnualSodium($lines[4]);
+        $meteo->setMechanicalMeanSodium($lines[5]);
+        $meteo->setMechanicalInterval($lines[6]);
+        $meteo->setMechanicalSodiumWater($lines[7]);
+
+        $meteo->setAutomaticAnnualSodium($lines[8]);
+        $meteo->setAutomaticMeanSodium($lines[9]);
+        $meteo->setAutomaticSprayInterval($lines[10]);
+        $meteo->setAutomaticSodiumWater($lines[11]);
+
+        $meteo->setExtTemperaturePosition($lines[12]);
+        $meteo->setExtTemperaturePosition2($lines[13]);
+        $meteo->setExtTemperatureAttenuation($lines[14]);
+        $meteo->setExtTemperatureAttenuation2($lines[15]);
+        $meteo->setExtTemperatureDifference($lines[16]);
+        $meteo->setExtHumidityPosition($lines[17]);
+        $meteo->setExtHumidityPosition2($lines[18]);
+        $meteo->setExtHumidityAttenuation($lines[19]);
+        $meteo->setExtHumidityAttenuation2($lines[20]);
+        $meteo->setExtHumidityDifference($lines[21]);
+
+        $meteo->setIntTemperaturePosition($lines[22]);
+        $meteo->setIntTemperaturePosition2($lines[23]);
+        $meteo->setIntTemperatureAttenuation($lines[24]);
+        $meteo->setIntTemperatureAttenuation2($lines[25]);
+        $meteo->setIntTemperatureDifference($lines[26]);
+        $meteo->setIntHumidityPosition($lines[27]);
+        $meteo->setIntHumidityPosition2($lines[28]);
+        $meteo->setIntHumidityAttenuation($lines[29]);
+        $meteo->setIntHumidityAttenuation2($lines[30]);
+        $meteo->setIntHumidityDifference($lines[31]);
 
     }
-*/
+
 
 
 }
