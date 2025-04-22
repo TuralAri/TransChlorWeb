b86379a1926bbcdfec9ec1f7105a8837e28f85b8
Tural <tural.ari@etu.univ-lyon1.fr>
2025-04-22 14:02:20 +0200

    Precalcul lors de l'upload d'un fichier météo

--- src/Entity/WeatherStation.php
+++ src/Entity/WeatherStation.php
@@ -26,6 +26,15 @@ class WeatherStation
     #[ORM\Column(length: 255)]
     private ?string $local_file_name = null;
 
+    #[ORM\Column]
+    private ?float $file_years = null;
+
+    #[ORM\Column]
+    private ?float $mechanical_annual_sodium = null;
+
+    #[ORM\Column]
+    private ?float $automatic_annual_sodium = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -75,6 +84,42 @@ class WeatherStation
     public function setLocalFileName(string $local_file_name): static
     {
         $this->local_file_name = $local_file_name;
+
+        return $this;
+    }
+
+    public function getFileYears(): ?float
+    {
+        return $this->file_years;
+    }
+
+    public function setFileYears(float $file_years): static
+    {
+        $this->file_years = $file_years;
+
+        return $this;
+    }
+
+    public function getMechanicalAnnualSodium(): ?float
+    {
+        return $this->mechanical_annual_sodium;
+    }
+
+    public function setMechanicalAnnualSodium(float $mechanical_annual_sodium): static
+    {
+        $this->mechanical_annual_sodium = $mechanical_annual_sodium;
+
+        return $this;
+    }
+
+    public function getAutomaticAnnualSodium(): ?float
+    {
+        return $this->automatic_annual_sodium;
+    }
+
+    public function setAutomaticAnnualSodium(float $automatic_annual_sodium): static
+    {
+        $this->automatic_annual_sodium = $automatic_annual_sodium;
 
         return $this;
     }
