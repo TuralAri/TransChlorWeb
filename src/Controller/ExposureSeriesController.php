8016f57e2edfa3ade9d2a0459533159ef5cd6cdd
Tural <tural.ari@etu.univ-lyon1.fr>
2025-04-24 12:08:26 +0200

    zip file receivable from route, downloaded into var/ressources/out

--- src/Controller/ExposureSeriesController.php
+++ src/Controller/ExposureSeriesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\ExposureSeries;
 use App\Entity\WeatherStation;
 use App\Form\ExposureSeriesFormType;
+use App\Form\GenerateExposureFormType;
 use App\Repository\ExposureSeriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpClient\HttpClient;
@@ -38,22 +39,47 @@ class ExposureSeriesController extends AbstractController
         $exposureSeriesForm->handleRequest($request);
 
         if ($exposureSeriesForm->isSubmitted() && $exposureSeriesForm->isValid()) {
-            $exposureSeriesFormData = $exposureSeriesForm->getData();
-            $formDataArray = $this->getFormData($exposureSeriesFormData);
-            $meteoFileName = $weatherStation->getFilename();
 
-            $response = $this->calculate($formDataArray,$meteoFileName);
-            if($response->getStatusCode() == 200){
-                $responseContent = json_decode(trim($response->getContent()), true);
-                $exposureSeries->setMechanicalInterventions($responseContent['mechanicalInterventions']);
-                $exposureSeries->setAutomaticSprays($responseContent['automaticSprays']);
-                $exposureSeries->setMechanicalThresholdTemperature($responseContent['mechanicalThresholdTemperature']);
-                $exposureSeries->setAutomaticThresholdTemperature($responseContent['automaticThresholdTemperature']);
-                $exposureSeriesForm = $this->createForm(ExposureSeriesFormType::class, $exposureSeries);
+            //BOUTON CALCULER CLIC
+            if ($exposureSeriesForm->get('submit')->isClicked()) {
+                $exposureSeriesFormData = $exposureSeriesForm->getData();
+                $formDataArray = $this->getFormData($exposureSeriesFormData, 0);
+                $meteoFileName = $weatherStation->getFilename();
 
-                //remplir
-                $this->addFlash('success', 'Calcul effectué avec succès');
+                //CALCUL DES 4 VALEURS MANQUANTES
+                $response = $this->calculate($formDataArray,$meteoFileName);
+                if($response->getStatusCode() == 200){
+                    $responseContent = json_decode(trim($response->getContent()), true);
+
+                    $exposureSeries->setMechanicalInterventions($responseContent['mechanicalInterventions']);
+                    $exposureSeries->setAutomaticSprays($responseContent['automaticSprays']);
+                    $exposureSeries->setMechanicalThresholdTemperature($responseContent['mechanicalThresholdTemperature']);
+                    $exposureSeries->setAutomaticThresholdTemperature($responseContent['automaticThresholdTemperature']);
+
+                    $exposureSeriesForm = $this->createForm(ExposureSeriesFormType::class, $exposureSeries);
+                    $this->addFlash('success', 'Calcul effectué avec succès');
+                }
             }
+
+            // BOUTON GENERER CLIC
+            elseif($exposureSeriesForm->get('generate')->isClicked()) {
+                $exposureSeriesFormData = $exposureSeriesForm->getData();
+                $formDataArray = $this->getFormData($exposureSeriesFormData, 2);
+                $exposureSeries = $exposureSeriesForm->getData();
+                $meteoFileName = $weatherStation->getFilename();
+
+                if($exposureSeries->getMechanicalInterventions() === null || $exposureSeries->getMechanicalThresholdTemperature() == null
+                    || $exposureSeries->getAutomaticThresholdTemperature() == null || $exposureSeries->getAutomaticSprays() == null){
+                    $this->addFlash('error', 'Des champs ne sont pas remplis, avez vous essayé de calculer les valeurs manquantes ?');
+                }
+
+                //Envoi du formulaire ainsi que du MeteoFile vers l'API C#
+                //ON ATTENDRA UN FICHIER ZIP EN REPONSE
+
+                $response = $this->generateExpositions($formDataArray, $meteoFileName);
+
+            }
+
         }
 
         return $this->render('exposure_series/generate.html.twig', [
@@ -68,7 +94,6 @@ class ExposureSeriesController extends AbstractController
         $uploadDirectory = $this->getParameter('upload_directory') . '/Ressources';
         $filePath1 = $uploadDirectory . '/MeteoFiles/' . $meteoFileName;
         $file1 = fopen($filePath1, 'r');
-
 
 //        $filePath2 = $this->getParameter('kernel.project_dir') . '/public/out/' . $fileName;
         $filePath2 = $uploadDirectory . '/out/' . $fileName;
@@ -102,43 +127,56 @@ class ExposureSeriesController extends AbstractController
         ];
     }
 
-
-    public function getFormData($formData): array
+    public function getFormData($formData, int $case): array
     {
-        return  [
-            'fileYears' => $formData->getFileYears(),
-            'sodiumChlorideConcentration' => $formData->getSodiumChlorideConcentration(),
-            'waterFilmThickness' => $formData->getWaterFilmThickness(),
-            'humidityThreshold' => $formData->getHumidityThreshold(),
-            'mechanicalAnnualSodium' => $formData->getMechanicalAnnualSodium(),
-            'mechanicalMeanSodium' => $formData->getMechanicalMeanSodium(),
-            'mechanicalInterval' => $formData->getMechanicalInterval(),
-            'mechanicalSodiumWater' => $formData->getMechanicalSodiumWater(),
-            'automaticAnnualSodium' => $formData->getAutomaticAnnualSodium(),
-            'automaticMeanSodium' => $formData->getAutomaticMeanSodium(),
-            'automaticSprayInterval' => $formData->getAutomaticSprayInterval(),
-            'automaticSodiumWater' => $formData->getAutomaticSodiumWater(),
-            'extTemperaturePosition' => $formData->getExtTemperaturePosition(),
-            'extTemperaturePosition2' => $formData->getExtTemperaturePosition2(),
-            'extTemperatureAttenuation' => $formData->getExtTemperatureAttenuation(),
-            'extTemperatureAttenuation2' => $formData->getExtTemperatureAttenuation2(),
-            'extTemperatureDifference' => $formData->getExtTemperatureDifference(),
-            'extHumidityPosition' => $formData->getExtHumidityPosition(),
-            'extHumidityPosition2' => $formData->getExtHumidityPosition2(),
-            'extHumidityAttenuation' => $formData->getExtHumidityAttenuation(),
-            'extHumidityAttenuation2' => $formData->getExtHumidityAttenuation2(),
-            'extHumidityDifference' => $formData->getExtHumidityDifference(),
-            'intTemperaturePosition' => $formData->getIntTemperaturePosition(),
-            'intTemperaturePosition2' => $formData->getIntTemperaturePosition2(),
-            'intTemperatureAttenuation' => $formData->getIntTemperatureAttenuation(),
-            'intTemperatureAttenuation2' => $formData->getIntTemperatureAttenuation2(),
-            'intTemperatureDifference' => $formData->getIntTemperatureDifference(),
-            'intHumidityPosition' => $formData->getIntHumidityPosition(),
-            'intHumidityPosition2' => $formData->getIntHumidityPosition2(),
-            'intHumidityAttenuation' => $formData->getIntHumidityAttenuation(),
-            'intHumidityAttenuation2' => $formData->getIntHumidityAttenuation2(),
-            'intHumidityDifference' => $formData->getIntHumidityDifference()
-        ];
+        if($case == 0){
+            return  [
+                'fileYears' => $formData->getFileYears(),
+                'sodiumChlorideConcentration' => $formData->getSodiumChlorideConcentration(),
+                'waterFilmThickness' => $formData->getWaterFilmThickness(),
+                'humidityThreshold' => $formData->getHumidityThreshold(),
+                'mechanicalAnnualSodium' => $formData->getMechanicalAnnualSodium(),
+                'mechanicalMeanSodium' => $formData->getMechanicalMeanSodium(),
+                'mechanicalInterval' => $formData->getMechanicalInterval(),
+                'mechanicalSodiumWater' => $formData->getMechanicalSodiumWater(),
+                'automaticAnnualSodium' => $formData->getAutomaticAnnualSodium(),
+                'automaticMeanSodium' => $formData->getAutomaticMeanSodium(),
+                'automaticSprayInterval' => $formData->getAutomaticSprayInterval(),
+                'automaticSodiumWater' => $formData->getAutomaticSodiumWater(),
+                'extTemperaturePosition' => $formData->getExtTemperaturePosition(),
+                'extTemperaturePosition2' => $formData->getExtTemperaturePosition2(),
+                'extTemperatureAttenuation' => $formData->getExtTemperatureAttenuation(),
+                'extTemperatureAttenuation2' => $formData->getExtTemperatureAttenuation2(),
+                'extTemperatureDifference' => $formData->getExtTemperatureDifference(),
+                'extHumidityPosition' => $formData->getExtHumidityPosition(),
+                'extHumidityPosition2' => $formData->getExtHumidityPosition2(),
+                'extHumidityAttenuation' => $formData->getExtHumidityAttenuation(),
+                'extHumidityAttenuation2' => $formData->getExtHumidityAttenuation2(),
+                'extHumidityDifference' => $formData->getExtHumidityDifference(),
+                'intTemperaturePosition' => $formData->getIntTemperaturePosition(),
+                'intTemperaturePosition2' => $formData->getIntTemperaturePosition2(),
+                'intTemperatureAttenuation' => $formData->getIntTemperatureAttenuation(),
+                'intTemperatureAttenuation2' => $formData->getIntTemperatureAttenuation2(),
+                'intTemperatureDifference' => $formData->getIntTemperatureDifference(),
+                'intHumidityPosition' => $formData->getIntHumidityPosition(),
+                'intHumidityPosition2' => $formData->getIntHumidityPosition2(),
+                'intHumidityAttenuation' => $formData->getIntHumidityAttenuation(),
+                'intHumidityAttenuation2' => $formData->getIntHumidityAttenuation2(),
+                'intHumidityDifference' => $formData->getIntHumidityDifference()
+            ];
+        }
+        elseif($case == 1){
+            return [
+            'mechanicalInterventions' => $formData->getMechanicalInterventions(),
+            'automaticSprays' => $formData->getAutomaticSprays(),
+            'mechanicalThresholdTemperature' => $formData->getMechanicalThresholdTemperature(),
+            'automaticThresholdTemperature' => $formData->getAutomaticThresholdTemperature()
+            ];
+        }
+        elseif($case == 2){
+            return array_merge($this->getFormData($formData,0),$this->getFormData($formData,1));
+        }
+        return [];
     }
 
     public function initAfterCalc(String $filePath): JsonResponse
@@ -156,8 +194,6 @@ class ExposureSeriesController extends AbstractController
 
         return new JsonResponse($data);
     }
-
-
 
     public function calculate(array $data , String $meteoFileName ): JsonResponse
     {
@@ -181,6 +217,45 @@ class ExposureSeriesController extends AbstractController
 
             unlink($outputFilePath);
             unlink($calcOutputFilePath);
+
+            if ($response->getStatusCode() === 200) {
+                // ✅ Retourne les données déjà en tableau
+                return $response;
+            } else {
+                return new JsonResponse([
+                    'error' => 'Erreur lors de l\'initialisation après calcul: ' . $response->getContent()
+                ], 500);
+            }
+
+        } else {
+            return new JsonResponse([
+                'error' => 'Erreur lors de l\'envoi du fichier: ' . $response->getContent()
+            ], 500);
+        }
+    }
+
+    public function generateExpositions(array $data, String $meteoFileName) : Response{
+        $uploadDirectory = $this->getParameter('upload_directory') . '/Ressources/';
+        $uniqueId = uniqid();
+
+        $outputFileName = 'form_meteo_input_' . $uniqueId . '.txt';
+        $outputFilePath = $uploadDirectory . 'out/' . $outputFileName;
+
+        $data = array_map(function($value) {
+            return is_float($value) || is_numeric($value) ? str_replace('.', ',', (string)$value) : $value;
+        }, $data);
+
+        $dataString = implode("\n", $data);
+        file_put_contents($outputFilePath, $dataString);
+
+        $response = $this->sendFileForCalc($meteoFileName, $outputFileName, 'export');
+        if ($response->getStatusCode() === 200) {
+            $responseContent = $response->getContent();
+            $zipOutputFileName = 'zip_expos' . $uniqueId . '.zip';
+            $zipOutputFilePath = $uploadDirectory . 'out/' . $zipOutputFileName;
+            file_put_contents($zipOutputFilePath, $responseContent);
+
+            unlink($outputFilePath); //SUPPRESSION DU FICHIER FORMULAIRE
 
             if ($response->getStatusCode() === 200) {
                 // ✅ Retourne les données déjà en tableau
