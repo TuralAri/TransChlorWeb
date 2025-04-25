c5d4210e17e2f6a826c5e85315b9e95161e93432
Tural <tural.ari@etu.univ-lyon1.fr>
2025-04-25 10:51:21 +0200

    tried creating api service for refactor, not working right now

--- src/Controller/WeatherStationController.php
+++ src/Controller/WeatherStationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\WeatherStation;
 use App\Form\WeatherStationFormType;
 use App\Repository\WeatherStationRepository;
+use App\Service\ApiService;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +20,11 @@ use function Symfony\Component\Translation\t;
 
 class WeatherStationController extends AbstractController
 {
+    private $apiService;
+    public function __construct(ApiService $apiService){
+        $this->apiService = $apiService;
+    }
+
     #[Route('/weatherstations', name: 'weather_stations')]
     public function index(WeatherStationRepository $repository)
     {
@@ -33,12 +39,69 @@ class WeatherStationController extends AbstractController
         ]);
     }
 
+//    #[Route('/weatherstations/add', name: 'weather_station_add')]
+//    public function add(EntityManagerInterface $em, Request $request, WeatherStationRepository $repository)
+//    {
+//        if(!$this->getUser()){
+//            return $this->redirectToRoute('index');
+//        }
+//        $weatherStation = new WeatherStation();
+//        $form = $this->createForm(WeatherStationFormType::class, $weatherStation);
+//        $form->handleRequest($request);
+//
+//        if($form->isSubmitted() && $form->isValid() && $request->getMethod() == 'POST'){
+//            $uploadMeteoFile = $form->get('filename')->getData();
+//
+//            $weatherStation = $form->getData();
+//            $weatherStation->setLocalFileName($uploadMeteoFile->getClientOriginalName());
+//            $weatherStation->setUploadedBy($this->getUser());
+//            $weatherStation->setUploadedAt(new \DateTimeImmutable('now'));
+//
+//            $newFileName = time() . '.' . $uploadMeteoFile->guessExtension();
+//
+//            $uploadDirectory = $this->getParameter('upload_directory') . '/Ressources/MeteoFiles';
+//
+//            try{
+//                $jsonResponse = $this->uploadAndPrecalculate($uploadMeteoFile, $newFileName);
+//                if($jsonResponse->getStatusCode() === 200){
+//                    $responseContent = $jsonResponse->getContent();
+//                    $values = json_decode($responseContent, true);
+//
+//                    if (isset($values[0]) && is_string($values[0])) {
+//                        $values = json_decode($values[0], true);
+//                    }
+//
+//                    $weatherStation->setFilename($newFileName);
+//                    $weatherStation->setFileYears($values['fileYears']);
+//                    $weatherStation->setMechanicalAnnualSodium($values['mechanicalAnnualSodium']);
+//                    $weatherStation->setAutomaticAnnualSodium($values['automaticAnnualSodium']);
+//
+//                    $em->persist($weatherStation);
+//                    $em->flush();
+//                    $this->addFlash('success', 'Fichier importé avec succès');
+//                }else{
+//                    $this->addFlash('error', 'Erreur lors de l\'importation du fichier');
+//                }
+//            }catch (FileException $e){
+//                //On verra ça plus tard
+//                $this->addFlash('error', 'Erreur lors de l\'upload du fichier');
+//            }
+//
+//            return $this->redirectToRoute('weather_stations');
+//        }
+//
+//        return $this->render('weatherstations/add.html.twig', [
+//            'form' => $form->createView(),
+//        ]);
+//    }
+
     #[Route('/weatherstations/add', name: 'weather_station_add')]
-    public function add(EntityManagerInterface $em, Request $request, WeatherStationRepository $repository)
+    public function add(EntityManagerInterface $em, Request $request)
     {
         if(!$this->getUser()){
             return $this->redirectToRoute('index');
         }
+
         $weatherStation = new WeatherStation();
         $form = $this->createForm(WeatherStationFormType::class, $weatherStation);
         $form->handleRequest($request);
@@ -56,9 +119,7 @@ class WeatherStationController extends AbstractController
             $uploadDirectory = $this->getParameter('upload_directory') . '/Ressources/MeteoFiles';
 
             try{
-//                $uploadMeteoFile->move($uploadDirectory, $newFileName);
-//                $values = json_decode($this->uploadAndPrecalculate($uploadMeteoFile, $newFileName));
-                $jsonResponse = $this->uploadAndPrecalculate($uploadMeteoFile, $newFileName);
+                $jsonResponse = $this->apiService->uploadAndPrecalculate($uploadMeteoFile, $newFileName);
                 if($jsonResponse->getStatusCode() === 200){
                     $responseContent = $jsonResponse->getContent();
                     $values = json_decode($responseContent, true);
---
+++ src/Service/ApiService.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class ApiService
+{
+    private $httpClient;
+    private $apiUrl;
+    private $meteoFilesDirectory;
+    private $outputDirectory;
+//    private $apiKey; //Inutile pour l'instant mais sera ajoutée plus tard pour plus de sécurité (si besoin)
+
+    public function __construct(HttpClientInterface $httpClient, string $apiUrl, string $uploadDir){
+        $this->httpClient = $httpClient;
+        $this->apiUrl = $apiUrl;
+        $this->meteoFilesDirectory = $uploadDir . '/Ressources/MeteoFiles';
+        $this->outputDir = $uploadDir . '/Ressources/out';
+    }
+
+    public function sendFile(string $filePath, string $route): Response
+    {
+        $file = fopen($filePath, 'r');
+
+        $response = $this->httpClient->request('POST', $this->apiUrl . $route, [
+            'body' => ['file' => $file]
+        ]);
+
+        return new Response($response->getContent(), $response->getStatusCode());
+    }
+
+    public function runTroubleshoots(string $filePath): array
+    {
+        return [
+            'troubleshoot1' => $this->sendFile($filePath, 'troubleshoot1'),
+            'troubleshoot2' => $this->sendFile($filePath, 'troubleshoot2'),
+        ];
+    }
+
+    public function extractPrecalcValues(string $filePath): array
+    {
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        if (count($lines) < 32) {
+            throw new \Exception('Fichier précalcul incomplet.');
+        }
+
+        return [
+            'fileYears' => floatval(str_replace(',', '.', $lines[0])),
+            'mechanicalAnnualSodium' => floatval(str_replace(',', '.', $lines[4])),
+            'automaticAnnualSodium' => floatval(str_replace(',', '.', $lines[8])),
+        ];
+    }
+
+    public function uploadAndPrecalculate(UploadedFile $file, string $newFileName): JsonResponse
+    {
+        try {
+            $filePath = $this->meteoFilesDirectory . '/' . $newFileName;
+            $file->move($this->meteoFilesDirectory, $newFileName);
+
+            $this->runTroubleshoots($filePath);
+
+            $response = $this->sendFile($filePath, 'precalcul');
+
+            if ($response->getStatusCode() !== 200) {
+                return new JsonResponse(['error' => 'Erreur API: ' . $response->getContent()], 500);
+            }
+
+            $outputPath = $this->outputDir . '/form_meteo_output_' . $newFileName;
+            file_put_contents($outputPath, $response->getContent());
+
+            $data = $this->extractPrecalcValues($outputPath);
+
+            unlink($outputPath); // Nettoyage
+
+            return new JsonResponse($data);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Erreur lors de l\'upload: ' . $e->getMessage()], 500);
+        }
+    }
+
+}
