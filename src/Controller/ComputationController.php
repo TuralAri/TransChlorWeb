1ac8dcf5cd91a2aad8f0c5c44a6c1d549594c31f
Tural Ari <tural.ari@etu.univ-lyon1.fr>
2025-05-02 10:13:52 +0200

    show last computing step

--- src/Controller/ComputationController.php
+++ src/Controller/ComputationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Computation;
 use App\Entity\ComputationResult;
 use App\Repository\ComputationRepository;
+use App\Repository\ComputationResultRepository;
 use App\Service\ApiService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -65,6 +66,39 @@ class ComputationController extends AbstractController
         $entityManager->flush();
 
         return $this->json(['success' => true],201);
+    }
+
+    #[Route('computation/{id}/show', name: 'show_computation')]
+    public function show(Computation $computation, EntityManagerInterface $entityManager, ComputationResultRepository $computationResultRepository)
+    {
+        $types = [
+            "temperature_potential",
+            "moisture_potential",
+            "moisture_content",
+            "total_chloride",
+            "free_chloride",
+            "ph"
+        ];
+
+        $graphData = [];
+
+        foreach ($types as $type) {
+            $result = $computationResultRepository->findOneBy(['computation' => $computation, 'type' => $type], ['id' => 'DESC']);
+            if (!$result) continue;
+
+            $graphData[] = [
+                'type' => $type,
+                'data' => array_map(fn($depth, $val) => ['x' => $depth, 'y' => $val], $result->getDepths(), $result->getComputedValues()),
+                'borderColor' => '#0000ff',
+                'fill' => false,
+                'tension' => 0.3
+            ];
+        }
+
+        return $this->render('computation/show.html.twig', [
+            'computation' => $computation,
+            'datasets' => $graphData,
+        ]);
     }
 
 
