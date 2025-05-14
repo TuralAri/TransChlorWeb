5110a7032e114455fb7959bc28144f80bf95f06d
Tural <tural.ari@etu.univ-lyon1.fr>
2025-05-14 15:44:48 +0200

    added a lot of translations for weather stations, exposures and exposureForm

--- src/Form/WeatherStationFormType.php
+++ src/Form/WeatherStationFormType.php
@@ -18,12 +18,12 @@ class WeatherStationFormType extends AbstractType
     {
         $builder
         ->add('filename', FileType::class, [
-            'label' => 'Importer un fichier météo',
+            'label' => 'weatherStation.addFile',
             'label_attr' => ['class' => 'text-base text-slate-900 font-medium mb-3 block'],
             'attr' => ['class' => 'block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400'],
         ])
         ->add('submit', SubmitType::class, [
-            'label' => 'Importer',
+            'label' => 'weatherStation.submit',
             'attr' => ['class' => 'class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 focus:outline-none cursor-pointer']
         ]);
     }
