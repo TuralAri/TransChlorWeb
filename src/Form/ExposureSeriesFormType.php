f2b6c8275cd4783b80639739e06abd60c6699ddb
Tural <tural.ari@etu.univ-lyon1.fr>
2025-05-15 10:51:55 +0200

    added english translations, fixed some translations that were not done

--- src/Form/ExposureSeriesFormType.php
+++ src/Form/ExposureSeriesFormType.php
@@ -64,13 +64,13 @@ class ExposureSeriesFormType extends AbstractType
             ->add('intHumidityDifference', NumberType::class, ['label' => 'exposureSeriesForm.HumidityDifference'])
 
             ->add('submit', SubmitType::class, [
-                'label' => 'Calculer',
+                'label' => 'exposureSeriesForm.calculateBtn',
                 'attr' => [
                     'class' => 'px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
                 ]
             ])
             ->add('generate', SubmitType::class, [
-                'label' => 'Générer les expositions',
+                'label' => 'exposureSeriesForm.generateBtn',
                 'attr' => [
                     'class' => 'px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
                 ]
