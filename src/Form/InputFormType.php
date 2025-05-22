045e4ed084075143ccab643849c627c351bbe670
Tural <tural.ari@etu.univ-lyon1.fr>
2025-05-22 09:32:02 +0200

    created formType for inputs

---
+++ src/Form/InputFormType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Exposure;
+use App\Entity\Input;
+use App\Entity\Material;
+use App\Entity\ProbabilisticLawParams;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InputFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('comment')
+            ->add('saveTimeTemperature')
+            ->add('saveTimeRelativeHumidity')
+            ->add('saveTimeWaterContent')
+            ->add('saveTimePh')
+            ->add('saveTimeFreeChlorures')
+            ->add('saveTimeTotalChlorures')
+            ->add('maxComputingTime')
+            ->add('computingTimeStep')
+            ->add('wallThickness')
+            ->add('elementsNumber')
+            ->add('meshType')
+            ->add('resultsDisplayTime')
+            ->add('capillarityTreatment')
+            ->add('leftEdgeCO2')
+            ->add('rightEdgeCO2')
+            ->add('leftEdgeCO2Choice')
+            ->add('rightEdgeCO2Choice')
+            ->add('thermalTransport')
+            ->add('waterTransport')
+            ->add('IonicTransport')
+            ->add('isWaterVaporTransportActivated')
+            ->add('isCapillarityTransportActivated')
+            ->add('isIonicTransportActivated')
+            ->add('isCarbonatationActivated')
+            ->add('exposureFile1', EntityType::class, [
+                'class' => Exposure::class,
+                'choice_label' => 'id',
+            ])
+            ->add('exposureFile2', EntityType::class, [
+                'class' => Exposure::class,
+                'choice_label' => 'id',
+            ])
+            ->add('material', EntityType::class, [
+                'class' => Material::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('vaporWaterTransport', EntityType::class, [
+                'class' => ProbabilisticLawParams::class,
+                'choice_label' => 'id',
+            ])
+            ->add('liquidWaterTransportCapillarity', EntityType::class, [
+                'class' => ProbabilisticLawParams::class,
+                'choice_label' => 'id',
+            ])
+            ->add('chlorideIonicTransport', EntityType::class, [
+                'class' => ProbabilisticLawParams::class,
+                'choice_label' => 'id',
+            ])
+            ->add('carbonation', EntityType::class, [
+                'class' => ProbabilisticLawParams::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Input::class,
+        ]);
+    }
+}
