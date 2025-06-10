e8252c9329cc8b4e0519dcf541f4045b29edf8e0
Tural <tural.ari@etu.univ-lyon1.fr>
2025-06-10 16:13:06 +0200

    modified entrypoint

--- docker/database-init-entrypoint.sh
+++ docker/database-init-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-while ! mysql -h db -u user -ppassword -e "SELECT 1" > /dev/null 2>&1; do
+while ! mysql -h db -u symfony -psymfony -e "SELECT 1" > /dev/null 2>&1; do
     echo "En attente de la base de données..."
     sleep 1
 done
