e9e6fe96444a812f33ff557a0f8f3a73a9e30938
Tural <tural.ari@etu.univ-lyon1.fr>
2025-06-12 15:12:18 +0200

    modified program.cs

--- TransChlorAPI/TransChlorApi/Program.cs
+++ TransChlorAPI/TransChlorApi/Program.cs
@@ -5,15 +5,17 @@ using TransChlorApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var webAddress = "http://web/api/"; //Replace "web" by localhost:8000 if you run the web app locally (without Docker)
+
 builder.Services.AddHttpClient<RandomComputingStrategy>(client =>
 {
-    client.BaseAddress = new Uri("http://web/api/");
+    client.BaseAddress = new Uri(webAddress);
     client.DefaultRequestHeaders.Add("Accept", "application/json");
 });
 
 builder.Services.AddHttpClient<RealComputingStrategy>(client =>
 {
-    client.BaseAddress = new Uri("http://web/api/");
+    client.BaseAddress = new Uri(webAddress);
     client.DefaultRequestHeaders.Add("Accept", "application/json");
 });
 
